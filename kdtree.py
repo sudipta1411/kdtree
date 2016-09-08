7eb62c2c56a7244c724a1956fb83b4558dea91ae
Your Name <you@example.com>
2016-09-09 01:52:12 +0530

    still buggy, abrupt recursion over limit

--- kdtree.py
+++ kdtree.py
@@ -6,11 +6,12 @@
 from random import randint
 import logging
 import sys
+import time
 
 __author__ = u'Sudipta Roy <csy157533@iitd.ernet.in>'
 logging.basicConfig(level=logging.DEBUG,
         format='%(asctime)s [%(levelname)s] %(message)s')
-sys.setrecursionlimit(10000)
+#sys.setrecursionlimit(10000)
 
 class Node(object) :
     def __init__(self, left=None, right=None, parent=None) :
@@ -47,6 +48,9 @@ class LeafNode(Node) :
     @data.setter
     def data(self,val) : self.__data = val
 
+    def __str__(self) :
+        return 'L(data={0})'.format(self.__data)
+
 class InternalNode(Node) :
     def __init__(self,axis=None, left=None, right=None, parent=None) :
         self.__axis = axis
@@ -58,6 +62,9 @@ class InternalNode(Node) :
     @axis.setter
     def axis(self,val) : self.__axis = val
 
+    def __str__(self) :
+        return 'I(axis={0})'.format(self.__axis)
+
 class KdTree(object) :
     '''A class implementing KdTree data structure'''
     def __init__(self,points=None,dim=2) :
@@ -67,7 +74,8 @@ class KdTree(object) :
                 any(pt is None or len(pt) != self.__dim for pt in points)) :
             raise ValueError("Dimension mismatch " + str(points) +
                     " and dimension %d"%dim)
-        self.__pts = points
+        self.__pts = list(set(points))
+        #self.__pts = list(set(points))
         self.__sorted_view = []
         self.__height = -1
 
@@ -75,25 +83,43 @@ class KdTree(object) :
         logging.debug("Building tree...")
         depth = 0
         if self.__pts is None or len(self.__pts) == 0 :
-            log.error("No points to build the KdTree on. Add points")
+            logging.error("No points to build the KdTree on. Add points")
             return
+        start = time.clock()
         self.__make_sorted_view()
+        print time.clock() - start, " seconds"
+        start = time.clock()
         self.__root = self.__build_tree(self.__sorted_view[0],depth)
-        print self.__root
+        print time.clock() - start, " seconds"
+        #print self.__root
 
-    def print_tree(self, node) : pass
+    def inorder(self,a, node=None) :
+        #for debugging purpose only
+        this = self.__root if node is None else node
+        # if this is not None :
+        if isinstance(this,LeafNode) :
+            a +=[str(this)]
+        else :
+            self.inorder(a,this.left)
+            a += [str(this)]
+            self.inorder(a,this.right)
+
 
     @property
     def root(self) : return self.__root
 
+    @property
+    def height(self, node = None) : return self.__height
+
     def __build_tree(self,pts,depth, parent=None) :
-        # logging.debug('depth : ' +str(depth))
+        logging.debug('depth : ' +str(depth))
+        self.__height = max(self.__height, depth)
         if pts == None or len(pts) == 0 :
             return LeafNode()
         if len(pts) == 1 :
             return LeafNode(data=pts[0], parent=parent)
         dim = self.__canon(depth)
-        # print 'dim :' + str(dim)
+        print 'dim :' + str(dim)
         max_pt = max(pts,key = lambda x :
             x[self.__canon(dim+1)])[self.__canon(dim+1)]
         min_pt = min(pts,key = lambda x :
@@ -108,10 +134,10 @@ class KdTree(object) :
         greater = [pt for pt in next_view if pt[dim]>=med
             and min_pt<=pt[self.__canon(dim+1)]<=max_pt
             and mn<=pt[dim]<=mx]
-        # print 'median : ' + str(med)
-        # print str(min_pt) + ':' + str(max_pt)
-        # print lesser
-        # print greater
+        print 'median : ' + str(med)
+        print str(min_pt) + ':' + str(max_pt)
+        print lesser
+        print greater
         node = InternalNode(axis = med, parent=parent)
         node.left = self.__build_tree(lesser,depth+1, parent=node)
         node.right = self.__build_tree(greater,depth+1, parent=node)
@@ -155,14 +181,16 @@ def create_random_point(upper_bound, nr_dim) :
 def main() :
     points = [(307, 75), (77, 92), (208, 146), (376, 63), (129, 248), (265, 258), (57, 410), (389, 456),
             (188, 128), (429, 214),(476, 132), (272, 485), (8, 415), (290, 124),
-            (407, 205), (166, 148)]
-    nr_pts = 4096
-    upper_bound = 10 * nr_pts
+            (407, 205), (166, 148), (166, 149)]
+    nr_pts = 8
+    upper_bound =10 * nr_pts
     points = create_random_points(upper_bound,nr_pts,2)
     # XXX : random 64 points sometimes failing
-    #points=[(16, 7, 214), (52, 407, 386), (65, 126, 83), (88, 204, 371)]
     kdtree = KdTree(points,dim=2)
     kdtree.build()
+    a=[]
+    kdtree.inorder(a)
+    print a
 
 if __name__ == '__main__':
     main()
