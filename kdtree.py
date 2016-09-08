1986c36f4b17817eb2607612e5463d9cf5808720
sudipta1411 <sdptroy3@gmail.com>
2016-09-08 13:48:40 +0530

    fix unlimited recursion depth issue

--- kdtree.py
+++ kdtree.py
@@ -13,9 +13,10 @@ logging.basicConfig(level=logging.DEBUG,
 sys.setrecursionlimit(10000)
 
 class Node(object) :
-    def __init__(self, left=None, right=None) :
+    def __init__(self, left=None, right=None, parent=None) :
         self.__left = left
         self.__right = right
+        self.__parent = parent
 
     @property
     def left(self) : return self.__left
@@ -29,10 +30,16 @@ class Node(object) :
     @right.setter
     def right(self,val) : self.__right = val
 
+    @property
+    def parent(self) : return self.__parent
+
+    @parent.setter
+    def parent(self,val) : self.__parent = val
+
 class LeafNode(Node) :
-    def __init__(self,data=None) :
+    def __init__(self,data=None, parent=None) :
         self.__data = data
-        super(LeafNode,self).__init__()
+        super(LeafNode,self).__init__(parent=parent)
 
     @property
     def data(self) : return self.__data
@@ -41,9 +48,9 @@ class LeafNode(Node) :
     def data(self,val) : self.__data = val
 
 class InternalNode(Node) :
-    def __init__(self,axis=None, left=None, right=None) :
+    def __init__(self,axis=None, left=None, right=None, parent=None) :
         self.__axis = axis
-        super(InternalNode,self).__init__(left=left,right=right)
+        super(InternalNode,self).__init__(left=left, right=right, parent=parent)
 
     @property
     def axis(self) : return self.__axis
@@ -54,7 +61,7 @@ class InternalNode(Node) :
 class KdTree(object) :
     '''A class implementing KdTree data structure'''
     def __init__(self,points=None,dim=2) :
-        self.__root = None
+        self.__root = Node()
         self.__dim = dim #dimension of points
         if(points is not None and
                 any(pt is None or len(pt) != self.__dim for pt in points)) :
@@ -62,6 +69,7 @@ class KdTree(object) :
                     " and dimension %d"%dim)
         self.__pts = points
         self.__sorted_view = []
+        self.__height = -1
 
     def build(self) :
         logging.debug("Building tree...")
@@ -73,24 +81,33 @@ class KdTree(object) :
         self.__root = self.__build_tree(self.__sorted_view[0],depth)
         print self.__root
 
-    def __build_tree(self,pts,depth) :
+    def __build_tree(self,pts,depth, parent=None) :
         logging.debug('depth : ' +str(depth))
         if len(pts) == 1 :
-            return LeafNode(data=pts[0])
+            return LeafNode(data=pts[0], parent=parent)
         dim = self.__canon(depth)
+        print 'dim :' + str(dim)
         max_pt = max(pts,key = lambda x :
             x[self.__canon(dim+1)])[self.__canon(dim+1)]
         min_pt = min(pts,key = lambda x :
             x[self.__canon(dim+1)])[self.__canon(dim+1)]
+        mx = max(pts,key = lambda x : x[dim])[dim] #max of current dimension
+        mn = min(pts,key = lambda x : x[dim])[dim] #min of current dimension
         med = KdTree.median(pts,dim)
         next_view = self.__sorted_view[self.__canon(dim+1)]
         lesser = [pt for pt in next_view if pt[dim]<med
-            and min_pt<=pt[self.__canon(dim+1)]<=max_pt]
+            and min_pt<=pt[self.__canon(dim+1)]<=max_pt
+            and mn<=pt[dim]<mx]
         greater = [pt for pt in next_view if pt[dim]>=med
-            and min_pt<=pt[self.__canon(dim+1)]<=max_pt]
-        node = InternalNode(axis = med)
-        node.left = self.__build_tree(lesser,depth+1)
-        node.right = self.__build_tree(greater,depth+1)
+            and min_pt<=pt[self.__canon(dim+1)]<=max_pt
+            and mn<=pt[dim]<=mx]
+        print 'median : ' + str(med)
+        print str(min_pt) + ':' + str(max_pt)
+        print lesser
+        print greater
+        node = InternalNode(axis = med, parent=parent)
+        node.left = self.__build_tree(lesser,depth+1, parent=node)
+        node.right = self.__build_tree(greater,depth+1, parent=node)
         return node
 
     def __canon(self,index) :
@@ -132,9 +149,9 @@ def create_random_point(upper_bound, nr_dim) :
 
 
 def main() :
-    points = [(307, 75), (77, 92), (208, 146), (376, 63), (129, 248), (265, 258), (57, 410), (389, 456)]
-            #(188, 128), (429, 214),(476, 132), (272, 485), (8, 415), (290, 124),
-            #(407, 205), (166, 148)]
+    points = [(307, 75), (77, 92), (208, 146), (376, 63), (129, 248), (265, 258), (57, 410), (389, 456),
+            (188, 128), (429, 214),(476, 132), (272, 485), (8, 415), (290, 124),
+            (407, 205), (166, 148)]
     #points = create_random_points(500,25,2)
     #points=[(16, 7, 214), (52, 407, 386), (65, 126, 83), (88, 204, 371)]
     kdtree = KdTree(points,dim=2)
