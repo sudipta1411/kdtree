d2caa288f278785df1969e781a53724639dd30c6
sudipta1411 <sdptroy3@gmail.com>
2016-09-12 15:44:41 +0530

    fixed recurse issue

--- kdtree.py
+++ kdtree.py
@@ -14,10 +14,11 @@ logging.basicConfig(level=logging.DEBUG,
 sys.setrecursionlimit(100)
 
 class Node(object) :
-    def __init__(self, left=None, right=None, parent=None) :
+    def __init__(self, left=None, right=None, parent=None, depth=-1) :
         self.__left = left
         self.__right = right
         self.__parent = parent
+        self.__depth = depth
 
     @property
     def left(self) : return self.__left
@@ -37,10 +38,16 @@ class Node(object) :
     @parent.setter
     def parent(self,val) : self.__parent = val
 
+    @property
+    def depth(self) : return self.__depth
+
+    @depth.setter
+    def depth(self,val) : self.__depth = val
+
 class LeafNode(Node) :
-    def __init__(self,data=None, parent=None) :
+    def __init__(self,data=None, parent=None, depth=-1) :
         self.__data = data
-        super(LeafNode,self).__init__(parent=parent)
+        super(LeafNode,self).__init__(parent=parent, depth=depth)
 
     @property
     def data(self) : return self.__data
@@ -51,10 +58,14 @@ class LeafNode(Node) :
     def __str__(self) :
         return 'L(data={0})'.format(self.__data)
 
+    def __repr__(self) :
+        return self.__str__()
+
 class InternalNode(Node) :
-    def __init__(self,axis=None, left=None, right=None, parent=None) :
+    def __init__(self,axis=None, left=None, right=None, parent=None, depth=-1) :
         self.__axis = axis
-        super(InternalNode,self).__init__(left=left, right=right, parent=parent)
+        super(InternalNode,self).__init__(left=left, right=right,
+                parent=parent, depth=depth)
 
     @property
     def axis(self) : return self.__axis
@@ -64,6 +75,33 @@ class InternalNode(Node) :
 
     def __str__(self) :
         return 'I(axis={0})'.format(self.__axis)
+
+    def __repr__(self) :
+        return self.__str__()
+
+class HyperRectangle(object) :
+    def __init__(self,dim,max_coord,min_coord) :
+        self.__dim = dim #dimension of the hyperrectangle
+        self.__max_coord = max_coord
+        self.__min_coord = min_coord
+
+    def inside(self,pt) :
+        if len(pt) != self.__dim :
+            raise ValueError("Dimension mismatch!!! hyperrectangle ", self.__dim,
+                    "while the point is ", pt)
+        max_min = zip(self.__min_coord,self.__max_coord)
+        for i in range(self.__dim) :
+            if pt[i]<max_min[i][0] or pt[i]>max_min[i][1]:
+                return False
+        return True
+
+    def 
+
+    def __str__(self) :
+        return "HyperRectangle {0}".format(list(zip(self.__min_coord,self.__max_coord)))
+
+    def __repr__(self) :
+        return self.__str__()
 
 class KdTree(object) :
     '''A class implementing KdTree data structure'''
@@ -93,65 +131,73 @@ class KdTree(object) :
         print time.clock() - start, " seconds"
         #print self.__root
 
-    def inorder(self,a, node=None) :
+    def inorder(self,a, node=None, inte=True) :
         #for debugging purpose only
         this = self.__root if node is None else node
         # if this is not None :
         if isinstance(this,LeafNode) :
-            a +=[str(this)]
+            a +=[this]
         else :
-            self.inorder(a,this.left)
-            a += [str(this)]
-            self.inorder(a,this.right)
+            self.inorder(a,this.left,inte)
+            if inte == True :
+                a += [this]
+            self.inorder(a,this.right,inte)
 
+    def query(self, maxes,mins) :
+        if rect is None :
+            raise ValueError("No Query range specified")
+        return self.__query(self.__root, HyperRectangle(maxes, mins, dim=self.__dim))
 
     @property
     def root(self) : return self.__root
 
     @property
-    def height(self, node = None) : return self.__height
+    def height(self) : return self.__height
 
     def __build_tree(self,pts,depth, parent=None) :
-        logging.debug('depth : ' +str(depth))
+        # logging.debug('depth : ' +str(depth))
         self.__height = max(self.__height, depth)
         if pts == None or len(pts) == 0 :
             return LeafNode()
         if len(pts) == 1 :
-            return LeafNode(data=pts[0], parent=parent)
+            return LeafNode(data=pts[0], parent=parent, depth=depth)
         dim = self.__canon(depth)
-        print 'dim :' + str(dim)
-        max_next = max(pts,key = lambda x :
-            x[self.__canon(dim+1)])[self.__canon(dim+1)]
-        min_next = min(pts,key = lambda x :
-            x[self.__canon(dim+1)])[self.__canon(dim+1)]
-        min_cur = pts[0][dim] #max(pts,key = lambda x : x[dim])[dim] #max of current dimension
-        max_cur = pts[len(pts)-1][dim] #min(pts,key = lambda x : x[dim])[dim] #min of current dimension
-        max_nn = max(pts,key = lambda x :
-            x[self.__canon(dim+2)])[self.__canon(dim+2)]
-        min_nn = min(pts,key = lambda x :
-            x[self.__canon(dim+2)])[self.__canon(dim+2)]
+        # print 'dim :' + str(dim)
+        # max_next = max(pts,key = lambda x :
+            # x[self.__canon(dim+1)])[self.__canon(dim+1)]
+        # min_next = min(pts,key = lambda x :
+            # x[self.__canon(dim+1)])[self.__canon(dim+1)]
+        # min_cur = pts[0][dim] #max(pts,key = lambda x : x[dim])[dim] #max of current dimension
+        # max_cur = pts[len(pts)-1][dim] #min(pts,key = lambda x : x[dim])[dim] #min of current dimension
+        # max_nn = max(pts,key = lambda x :
+            # x[self.__canon(dim+2)])[self.__canon(dim+2)]
+        # min_nn = min(pts,key = lambda x :
+            # x[self.__canon(dim+2)])[self.__canon(dim+2)]
         med = KdTree.median(pts,dim)
-        next_view = self.__sorted_view[self.__canon(dim+1)]
-        lesser = [pt for pt in next_view if pt[dim]<med
-            and min_next<=pt[self.__canon(dim+1)]<=max_next
-            and min_cur<=pt[dim]<=max_cur
-            and min_nn<=pt[self.__canon(dim+2)]<=max_nn]
-        greater = [pt for pt in next_view if pt[dim]>=med
-            and min_next<=pt[self.__canon(dim+1)]<=max_next
-            and min_cur<=pt[dim]<=max_cur
-            and min_nn<=pt[self.__canon(dim+2)]<=max_nn]
-        # lesser = [pt for pt in pts if pt[dim]<med]
-        # lesser.sort(key=lambda x : x[self.__canon(dim+1)])
-        # greater = [pt for pt in pts if pt[dim]>=med]
-        # greater.sort(key=lambda x : x[self.__canon(dim+1)])
-        print 'median : ' + str(med)
-        print str(min_cur) + ':' + str(max_cur)
-        print lesser
-        print greater
-        node = InternalNode(axis = med, parent=parent)
+        # next_view = self.__sorted_view[self.__canon(dim+1)]
+        # lesser = [pt for pt in next_view if pt[dim]<med
+            # and min_next<=pt[self.__canon(dim+1)]<=max_next
+            # and min_cur<=pt[dim]<=max_cur
+            # and min_nn<=pt[self.__canon(dim+2)]<=max_nn]
+        # greater = [pt for pt in next_view if pt[dim]>=med
+            # and min_next<=pt[self.__canon(dim+1)]<=max_next
+            # and min_cur<=pt[dim]<=max_cur
+            # and min_nn<=pt[self.__canon(dim+2)]<=max_nn]
+        lesser = [pt for pt in pts if pt[dim]<med]
+        lesser.sort(key=lambda x : x[self.__canon(dim+1)])
+        greater = [pt for pt in pts if pt[dim]>=med]
+        greater.sort(key=lambda x : x[self.__canon(dim+1)])
+        # print 'median : ' + str(med)
+        # print str(min_cur) + ':' + str(max_cur)
+        # print lesser
+        # print greater
+        node = InternalNode(axis = med, parent=parent, depth=depth)
         node.left = self.__build_tree(lesser,depth+1, parent=node)
         node.right = self.__build_tree(greater,depth+1, parent=node)
         return node
+
+    def __query(self, node, rect) :
+        pass
 
     def __canon(self,index) :
         return index % self.__dim #increment/decrement of index must be modulo @dim
@@ -166,7 +212,7 @@ class KdTree(object) :
         for i in range(self.__dim) :
             self.__sorted_view.append(sorted(self.__pts,
                 key = lambda x : x[i]))
-        print self.__sorted_view
+        # print self.__sorted_view
 
     @staticmethod
     def median(lst,index) :
@@ -196,19 +242,17 @@ def main() :
     upper_bound =10 * nr_pts
     points = create_random_points(upper_bound,nr_pts,2)
     points3d = create_random_points(upper_bound,nr_pts,3)
-    #points3d = [(56, 68, 74), (83, 16, 5), (87, 76, 89), (16, 46, 59), (73, 37, 12),
-    #        (59, 27, 82), (71, 10, 13), (73, 61, 53), (62, 26, 85)]
-    print points3d
+    points3d = [(56, 68, 74), (83, 16, 5), (87, 76, 89), (16, 46, 59), (73, 37, 12),
+            (59, 27, 82), (71, 10, 13), (73, 61, 53), (62, 26, 85)]
+    # print points3d
     # XXX : random 64 points sometimes failing
     kdtree = KdTree(points3d,dim=3)
-    try:
-        kdtree.build()
-        print "Height of the tree is : " , kdtree.height
-        a=[]
-        #kdtree.inorder(a)
-        print a
-    except RuntimeError:
-        print 'maximum recursion depth exceeded while calling a Python object'
+    kdtree.build()
+    print "Height of the tree is : " , kdtree.height
+    a=[]
+    kdtree.inorder(a,inte=False)
+    print a
+    print "depth of the tree is : " , kdtree.root.depth
 
 if __name__ == '__main__':
     main()
